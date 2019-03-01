df8c091aefe8fc594cacf1b3fb7fb35cd5d128b8
Eilif Mikkelsen <9872160+eilifm@users.noreply.github.com>
2019-02-28 16:08:08 -0800

    fixing some simple bugs

--- init_db.sh
+++ init_db.sh
@@ -2,4 +2,4 @@
 
 echo ".quit" | sqlite3 db/tartan.db -init db/schema.sql
 echo ".quit" | sqlite3 db/tartan.db -init db/migration/20170917.sql
-
+echo ".quit" | sqlite3 db/tartan.db -init db/migration/20190228.sql
